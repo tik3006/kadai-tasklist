ca068c76464c25342a02a8c210e286c271df2a76
tik <bump130guitar@gmail.com>
2019-08-28 08:46:25 +0000

    login

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ group :production do
   gem 'pg', '>= 0.18', '< 2.0'
 end
 gem 'kaminari'
+gem 'pry-byebug', group: :development
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+ private
+
+  def require_user_logged_in
+   #ログインしていない場合ログインページへと遷移させる
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
