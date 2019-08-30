b426496b1b0265fe2b95e0ae4519efcd338faea7
tik <bump130guitar@gmail.com>
2019-08-30 08:35:40 +0000

    delete file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,11 @@
 class TasksController < ApplicationController
-  #before_action :set_task, only: [:show, :edit,:destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy,:show,:edit,:update]
   
   #すべてのタスクの取得表示
   def index
     if logged_in?
-     @tasks = current_user.tasks.build  # form_with 用
      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-     #@tasks = Task.all
     end
   end
   
@@ -54,12 +51,6 @@ class TasksController < ApplicationController
   
   
   private
-
- #ログインユーザーの情報を取得しインスタンス変数に格納する
-  #def set_task
-   # @task = Task.find(id: params[:id])
-    #@task = current_user.tasks.find_by(id: params[:id])
- # end
 
   def task_params
     params.require(:task).permit(:content, :status)
